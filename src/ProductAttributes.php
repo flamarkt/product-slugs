a4249a772873f50482033ecd974007e776abf270
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-08-20 23:35:01 +0200

    Already fixing things

--- src/ProductAttributes.php
+++ src/ProductAttributes.php
@@ -2,12 +2,12 @@
 
 namespace Flamarkt\ProductSlugs;
 
-use Flamarkt\Core\Api\Serializer\ProductSerializer;
+use Flamarkt\Core\Api\Serializer\BasicProductSerializer;
 use Flamarkt\Core\Product\Product;
 
 class ProductAttributes
 {
-    public function __invoke(ProductSerializer $serializer, Product $product): array
+    public function __invoke(BasicProductSerializer $serializer, Product $product): array
     {
         return [
             // For now we show this value to everyone, it doesn't really matter since it is public anyway
